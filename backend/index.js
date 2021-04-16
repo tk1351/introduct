67033f141c7c0e6d9ac27f91f82175c4be4294ac
tk1351 <takakikitsui0111@gmail.com>
2021-04-16 15:37:28 +0900

    connected mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const PORT = process.env.PORT || 8080
+const connectDB = require('./config/db')
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
