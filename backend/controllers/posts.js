b6c1dfe5ea14d1556fae2317b06b9f5e9f3fea8c
tk1351 <takakikitsui0111@gmail.com>
2021-04-19 14:28:58 +0900

    added auth controller and createPost func

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const Post = require('../models/Post')
+const User = require('../models/User')
+const { validationResult } = require('express-validator')
 
 module.exports = {
   testRouter: (req, res) => {
@@ -27,6 +29,48 @@ module.exports = {
       if (err.kind === 'ObjectId') {
         return res.status(404).json({ msg: '投稿がありません' })
       }
+      res.status(500).send('Server Error')
+    }
+  },
+  getPostsByUserId: async (req, res) => {
+    try {
+      const posts = await Post.find({ user: req.params.user_id }).sort({
+        createdAt: -1,
+      })
+      if (!posts) {
+        return res.status(400).json({ msg: '投稿がありません' })
+      }
+      res.json(posts)
+    } catch (err) {
+      console.error(err.message)
+      if (err.kind === 'ObjectId') {
+        return res.status(404).json({ msg: '投稿がありません' })
+      }
+      res.status(500).send('Server Error')
+    }
+  },
+  createPost: async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password')
+      const newPost = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        url: req.body.url,
+        user: req.body.id,
+        name: user.name,
+        avatar: user.avatar,
+      })
+
+      const post = await newPost.save()
+      res.json(post)
+    } catch (err) {
+      console.error(err.message)
       res.status(500).send('Server Error')
     }
   },
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const postsController = require('../controllers/posts')
+const auth = require('../middleware/auth')
+const { check } = require('express-validator')
 
 router.get('/test', postsController.testRouter)
 router.get('/', postsController.getAllPosts)
 router.get('/:post_id', postsController.getPostById)
+router.get('/profile/:user_id', postsController.getPostsByUserId)
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'タイトルが必要です').not().isEmpty(),
+      check('text', '本文が必要です').not().isEmpty(),
+    ],
+  ],
+  postsController.createPost
+)
 
 module.exports = router
